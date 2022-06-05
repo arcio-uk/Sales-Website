65da94fb981459c261349c828b621cd51a0b57c8
James <jamesarnott2@gmail.com>
2022-06-06 00:27:02 +0100

    style: changes to the mobile icon box sizing

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,7 @@ const App = () => (
       <Reliability />
     </Carosel>
     <div className="flex w-full justify-center">
-      <div className="xl:grid-cols-4 grid md:grid-cols-2 grid-cols-1 gap-5 md:w-3/5 4/5">
+      <div className="xl:grid-cols-4 grid grid-cols-2 md:gap-5 md:w-3/5">
         <IconBox title="Attendance Tracking" icon={<FaCalendarCheck className="w-full h-24 fill-slate-800" />} />
         <IconBox title="Timetable Management" icon={<BsFillCalendarWeekFill className="w-full h-24 fill-slate-800" />} />
         <IconBox title="Easy Data Imports and Exports" icon={<MdOutlineImportExport className="w-full h-24 fill-slate-800" />} />
--- src/ui/IconBox/IconBox.tsx
+++ src/ui/IconBox/IconBox.tsx
@@ -9,11 +9,11 @@ type IconBoxProps = {
   icon: ReactNode;
 }
 const IconBox = ({ title, icon, children }: IconBoxProps) => (
-  <div className="text-center bg-white m-5 rounded-2xl shadow-2xl flex-col flex min-w-fit">
-    <div className="m-2 overlay flex-2">
+  <div className="text-center bg-white rounded-2xl shadow-2xl flex-col flex min-w-fit m-2">
+    <div className="md:m-2 m-1 overlay flex-2">
       {icon}
     </div>
-    <div className="text-3xl  m-2 flex-1 flex flex-col">
+    <div className="text-3xl md:m-2 m-1 flex-1 flex flex-col">
       <div className="flex-center m-auto">
         {title}
       </div>
