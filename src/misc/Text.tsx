a5cdc0719eeb0f67d177d28cf1d7f3b1d57170a9
James <jamesarnott2@gmail.com>
2022-06-05 15:20:18 +0100

    feat: created text.tsx, for multilingual support!

---
+++ src/misc/Text.tsx
@@ -0,0 +1,19 @@
+/**
+ * Use this for language codes:
+ * http://www.lingoes.net/en/translator/langcode.htm
+ */
+const Text = {
+  en: {
+    welcome: 'Arcio Systems',
+    caroselSlides: [],
+    explanationBoxes: [],
+  },
+  es: {
+    welcome: 'Sistemas Arcio',
+  },
+  it: {
+    welcome: 'Sistemi Arcio',
+  },
+};
+
+export default Text;
