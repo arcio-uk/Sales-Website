2c5ac1a00aff983fe6649ddd9bff23299fb2b827
James <jamesarnott2@gmail.com>
2022-06-06 13:44:53 +0100

    chore: added tailwind animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable global-require */
 module.exports = {
   content: [
     './index.html',
@@ -9,7 +10,20 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-animatecss')({
+      classes: ['animate__animated', 'animate__fadeIn', 'animate__bounceIn', 'animate__lightSpeedOut'],
+      settings: {
+        animatedSpeed: 1000,
+        heartBeatSpeed: 1000,
+        hingeSpeed: 2000,
+        bounceInSpeed: 750,
+        bounceOutSpeed: 750,
+        animationDelaySpeed: 1000,
+      },
+      variants: ['responsive', 'hover', 'reduced-motion'],
+    }),
+  ],
   include: [
     'src',
     'types',
