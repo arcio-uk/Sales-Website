31a6728e4f9062c30bb357951e7ac306bf77acb8
James <jamesarnott2@gmail.com>
2022-06-06 19:00:13 +0100

    fix: flowing gradients!

--- src/ui/Carosel/Slides/Flexibility.tsx
+++ src/ui/Carosel/Slides/Flexibility.tsx
@@ -7,7 +7,7 @@ import Slide from './Slide';
 const staticText = Text.caroselSlides.flexibility;
 
 const Flexibility = () => (
-  <Slide title={staticText.title} text={staticText.text} className="bg-gradient-to-r from-orange-500 to-pink-600">
+  <Slide title={staticText.title} text={staticText.text} className="bg-gradient-to-r from-fuchsia-500 to-pink-500">
     <SiOpenaccess className="w-36 h-36 flex-1 m-auto xl:my-auto my-3" />
   </Slide>
 );
