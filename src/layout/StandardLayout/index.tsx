f7eb9463eebd3631982d437d0346db9237639992
James <jamesarnott2@gmail.com>
2022-06-07 11:33:17 +0100

    fix: the footer is always at the bottom now

--- src/layout/StandardLayout/index.tsx
+++ src/layout/StandardLayout/index.tsx
@@ -8,8 +8,8 @@ import './index.css';
 const staticText = Text.standardLayout;
 
 const StandardLayout = () => (
-  <div>
-    <div className="h-16 bg-gradient-to-r from-orange-400 to-red-400 fixed top-0 w-full text-4xl justify-right flex-col space-x-5 text-white z-10">
+  <div className="flex flex-col h-screen justify-between">
+    <header className="h-16 bg-gradient-to-r from-orange-400 to-red-400 fixed top-0 w-full text-4xl justify-right flex-col space-x-5 text-white z-10">
       <div className="flex-1 inline-block align-middle">
         Helooooooooo, I&#39;m a navigation bar!
       </div>
@@ -17,14 +17,18 @@ const StandardLayout = () => (
         Helooooooooo, I&#39;m a navigation bar!
       </div>
 
+    </header>
+
+    <div className="main">
+      <div className="h-16" />
+      <Outlet />
     </div>
-    <div className="h-16" />
-    <Outlet />
-    <div className="text-center p-3 bg-gradient-to-r from-orange-400 to-red-400 text-white font-semibold z-10 bottom-0 absolute w-full">
+
+    <footer className="text-center p-3 bg-gradient-to-r from-orange-400 to-red-400 text-white font-semibold z-10 bottom-0 relative w-full">
       {staticText.copyright.first}
       <AiFillHeart className="fill-red-600 inline align-middle" />
       {staticText.copyright.second}
-    </div>
+    </footer>
   </div>
 );
 
