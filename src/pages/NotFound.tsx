496004d0ff7b4608ec79bb350bef06cfeca7deab
James <jamesarnott2@gmail.com>
2022-06-07 11:38:32 +0100

    feat: 404 fade in

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 
 const NotFound = () => (
-  <div className="flex justify-center w-full h-[60rem]">
+  <div className="flex justify-center w-full h-[60rem] animate__animated animate__fadeIn">
     <div className="text-3xl bg-white md:w-3/5 my-auto p-5 rounded-2xl shadow-2xl">
       Sorry, the page you&#39;re looking for doesn&#39;t exist :(
       <Link to="/" className="block mt-10 cursor-pointer text-blue-500 underline select-none">
