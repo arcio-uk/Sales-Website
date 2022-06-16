c0608db664f4a28114a48ddbb0ef3cf208c50d5b
James <jamesarnott2@gmail.com>
2022-06-16 17:41:35 +0100

    feat: added optional social medias

--- src/pages/AboutUs/ProfileCard/index.tsx
+++ src/pages/AboutUs/ProfileCard/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   BsDiscord, BsLinkedin, BsTwitter, BsYoutube,
 } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
 import Rick from './rick.png';
 
 interface ProfileCardProps {
@@ -9,6 +10,12 @@ interface ProfileCardProps {
 		name: string,
 		role?: string,
 		description: string,
+		socials?: {
+			linkedIn?: string,
+			youtube?: string,
+			discord?: string,
+			twitter?: string,
+		}
 	}
 }
 
@@ -23,16 +30,35 @@ const ProfileCard = ({ person }: ProfileCardProps) => (
 		border-slate-500`}
   >
     <img src={Rick} className="rounded-t-2xl" />
-    <div className="flex w-full justify-center content-center m-1">
-      <BsLinkedin className="mx-1" />
-      <BsYoutube className="mx-1" />
-      <BsDiscord className="mx-1" />
-      <BsTwitter className="mx-1" />
+    {person.socials && (
+    <div className="flex justify-center content-center border-2 border-slate-500 max-w-full p-1 m-1 rounded-md">
+      {person.socials.linkedIn && (
+      <a href={person.socials.linkedIn} target="_blank" rel="noreferrer">
+        <BsLinkedin className="mx-1" />
+      </a>
+      )}
+      {person.socials.youtube && (
+      <a href={person.socials.youtube} target="_blank" rel="noreferrer">
+        <BsYoutube className="mx-1" />
+      </a>
+      )}
+      {person.socials.discord && (
+      <a href={person.socials.discord} target="_blank" rel="noreferrer">
+        <BsDiscord className="mx-1" />
+      </a>
+      )}
+      {person.socials.twitter && (
+      <a href={person.socials.twitter} target="_blank" rel="noreferrer">
+        <BsTwitter className="mx-1" />
+      </a>
+      )}
     </div>
+    )}
+
     <div className="text-center text-3xl">
       {person.name}
     </div>
-    <div className="border-2 border-black w-5/6 mx-auto my-3 rounded-full" />
+    <div className="border-b-2 h-1 border-slate-500 w-11/12 mx-auto my-2 rounded-full" />
     <div className="text-lg m-1">
       {person.description}
     </div>
