bc46bafddfac4bbc57d83b47fc854790a2106342
James <jamesarnott2@gmail.com>
2022-07-03 11:21:22 +0100

    refactor: renamed home to MISsalesPage, moved it's slides in it's folder

--- src/pages/MISsalesPage/index.tsx
+++ src/pages/MISsalesPage/index.tsx
@@ -2,12 +2,12 @@ import Carosel from '@/ui/Carosel/Carosel';
 import ContactUs from '@/ui/ContactUs';
 import {
   Speed, Security, Reliability, Flexibility,
-} from '@/ui/Carosel/Slides';
+} from './Slides';
 import TitleBar from '@/ui/TitleBar';
 import { IconBoxContainer } from '@/ui/IconBox';
 import Text from '@/misc/Text';
 
-const Home = () => (
+const MISsalesPage = () => (
   <div className="justify-center content-center h-full w-full flex-col">
     <div className="flex">
       <TitleBar text={Text.welcome} className="w-full bg-white m-5 rounded-full shadow-2xl" />
@@ -23,4 +23,4 @@ const Home = () => (
   </div>
 );
 
-export default Home;
+export default MISsalesPage;
